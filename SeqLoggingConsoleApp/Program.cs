48786bae7bc1f0c2d5f1eb975805be0a0a1846e1
Mehran Khan <mehranumr@gmail.com>
2023-12-08 16:55:18 +0500

    first commit

---
+++ SeqLoggingConsoleApp/Program.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.Logging;
+
+// Instantiate LoggerFactory Object using SEQ
+using ILoggerFactory factory = LoggerFactory.Create(builder => builder.AddSeq());
+
+// Creating logger object
+ILogger logger = factory.CreateLogger("Program");
+
+// time to logging
+logger.LogInformation("Hello World! Logging is {Description}.", "fun");
+logger.LogInformation("Hello from SEQ");
